10f75c10ba350443ef65e2dc884f2dbf9200f6f0
faridomarzadeh <farid.omarzadeh29@gmail.com>
2020-03-22 19:25:05 -0400

    small chnages in main controller

---
+++ src/application/controller/MainController.java
@@ -0,0 +1,12 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+
+	@FXML
+	private void close(){
+		System.exit(0);
+	}
+
+}
--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module smartgcc {
-    requires transitive javafx.controls;
-    requires javafx.fxml;
-
-    opens application to javafx.fxml;
-    opens application.controller to javafx.fxml;
-    exports application;
-}

93b8a933a643f9e43e468de4eee99478848ebb12
nithin anchuri <nithin171@gmail.com>
2020-04-06 02:03:51 -0400

    Added Help panel

--- src/application/controller/HelpController.java
+++ src/application/controller/HelpController.java
@@ -19,14 +19,40 @@ public class HelpController implements Initializable{
     
 	@FXML
     void compileProject(MouseEvent event) {
-		text.setText("Compile a project");
+		text.setText("1. Create a project \n\n2. Click Build icon \n\n3. View the output at the provided directoty location ");
     }
 	
 	@FXML
     void createProject(MouseEvent event) {
-		text.setText("Create a project");
+		text.setText("1. Inside SmartGCC select the menu item File > New >Project.... or click on the new project icon to open the New Project wizard \n\n2. Provide the location of the directory where you can check the results \n\n3. Provide the project name and click the + symbol to the file you want work on \n\n4. Click Create");
     }
 
+	@FXML
+	void buildProject(MouseEvent event) {
+		text.setText("1. Create a project \n\n2. Click the Build button or press ctrl+b");
+	}
+	
+
+    @FXML
+    void saveProject(MouseEvent event) {
+		text.setText("1. Create a project \n\n 2. Click on save icon on main panel \n\n3. Provide location  \n\n4. Click save in the dialog box ");
+    }
+    
+    @FXML
+    void options(MouseEvent event) {
+		text.setText("1. Create a project \n\n2. Build the code using Build icon on the main panel \n\n3. Choose the required options \n\n4. Click Build");
+    }
+    
+    @FXML
+    void userType(MouseEvent event) {
+		text.setText("Change the user type by clicling change user type icon on the main panel and select your type of user");
+    }
+
+    @FXML
+    void compilerOptions(MouseEvent event) {
+    	text.setText("");
+    }
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
